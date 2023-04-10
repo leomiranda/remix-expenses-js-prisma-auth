0abbddd7e25add91fab4286c084c1e1851883ee7
Leo Miranda <leonardomir@gmail.com>
2023-04-09 20:38:18 -0400

    feat: raw content

--- app/routes/expenses.raw.jsx
+++ app/routes/expenses.raw.jsx
@@ -1,7 +1,30 @@
-export default function ExpensesRawPage() {
-	return (
-		<div>
-			<h1>Expenses Raw Page</h1>
-		</div>
-	);
+const DUMMY_EXPENSES = [
+	{
+		id: 'e1',
+		title: 'Toilet Paper',
+		amount: 94.12,
+		date: new Date(2022, 7, 14),
+	},
+	{
+		id: 'e2',
+		title: 'New TV',
+		amount: 799.49,
+		date: new Date(2022, 2, 12),
+	},
+	{
+		id: 'e3',
+		title: 'Car Insurance',
+		amount: 294.67,
+		date: new Date(2022, 2, 28),
+	},
+	{
+		id: 'e4',
+		title: 'New Desk (Wooden)',
+		amount: 450,
+		date: new Date(2022, 5, 12),
+	},
+];
+
+export function loader() {
+	return DUMMY_EXPENSES;
 }
